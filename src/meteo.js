95aeb7fad9e8905a0b0c3fed11e126d82fd92f55
Devfection <8330@cygnific.com>
2026-05-26 13:33:36 +0200

    basic js behaviour, no API

--- src/meteo.js
+++ src/meteo.js
@@ -1,1 +1,11 @@
+function handleSearchSubmit(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form");
+    let cityElement = document.querySelector("#current-city");
+    cityElement.innerHTML = searchInput.value;
+}
 
+
+
+let searchFormElement = document.querySelector("#form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
