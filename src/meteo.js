05af2738eeba940754da8ecb99ad6a687b72c763
Devfection <8330@cygnific.com>
2026-05-27 15:09:04 +0200

    fixed some css bugs and added the forecast for 5 days in the future

--- src/meteo.js
+++ src/meteo.js
@@ -52,24 +52,39 @@ function handleSearchSubmit(event){
   searchCity(searchInput.value)
   }
 
+function formatDay(timestamp){
+  let date= new Date (timestamp * 1000 );
+  let days = ["Sun", 
+        "Mon", 
+        "Tue", 
+        "Wed", 
+        "Thu", 
+        "Fri", 
+        "Sat"];
+        return days[date.getDay()];
+}
 
 function displayForecast(response){
   console.log(response.data);
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
   let forecastHtml = "";
     
-  days.forEach(function (day){
+  response.data.daily.forEach(function (day, index){
+    if(index< 5){
         forecastHtml  = 
             forecastHtml +
             `
             <div class="forecast-day">  
-            <div class="weather-forecast-day"> ${day} </div>
-            <div class="weather-forecast-icon">🌤  </div>
+            <div class="weather-forecast-day"> ${formatDay(day.time)} </div>
+            <div><img class="weather-forecast-icon" src="${day.condition.icon_url}">  </div>
             <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-temperature"> <strong>15°C </strong></div><div class="weather-forecast-temperature">10°C</div>
+            <div class="weather-forecast-temperature"> <strong>${Math.round(day.temperature.maximum)}°C </strong>
+            </div>
+            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°C
+            </div>
               </div>
             </div>
             `;
+            }
   }) ;
   let forecastElement = document.querySelector("#forecast");
     forecastElement.innerHTML = forecastHtml;
