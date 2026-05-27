0ff4d8b4892ad0bd42d436492abc89ec74f18128
Devfection <8330@cygnific.com>
2026-05-27 14:32:03 +0200

    get forecast from the API for the rest of the week from the city search result

--- src/meteo.js
+++ src/meteo.js
@@ -18,7 +18,7 @@ function refreshWeather(response){
     descriptionElement.innerHTML = response.data.condition.description;
     cityElement.innerHTML = response.data.city ;
     temperatureElement.innerHTML = Math.round(temperature);
-   
+   getForecast(response.data.city);
 }
 
 function formatDate(date){
@@ -46,13 +46,43 @@ function searchCity(city){
     axios.get(apiURL).then(refreshWeather);
 }
 function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form");
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form");
     
-    searchCity(searchInput.value)
-    }
+  searchCity(searchInput.value)
+  }
 
 
+function displayForecast(response){
+  console.log(response.data);
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  let forecastHtml = "";
+    
+  days.forEach(function (day){
+        forecastHtml  = 
+            forecastHtml +
+            `
+            <div class="forecast-day">  
+            <div class="weather-forecast-day"> ${day} </div>
+            <div class="weather-forecast-icon">🌤  </div>
+            <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature"> <strong>15°C </strong></div><div class="weather-forecast-temperature">10°C</div>
+              </div>
+            </div>
+            `;
+  }) ;
+  let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+}
+
+function getForecast(city){
+  let apiKey = "61ff3ct11d507f83e0fcob41d8ed7a85";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+};
+
+
+//displayForecast(); 
 let searchFormElement = document.querySelector("#form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
